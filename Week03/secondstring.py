af61decaae4d3334ae2670f7fba60ce0c904dcdb
martincusack979 <cusackmartin@gmail.com>
2022-02-07 20:04:05 +0000

    Week03

---
+++ Week03/secondstring.py
@@ -0,0 +1,7 @@
+# Write a program that asks a user to input a string and outputs every second letter in reverse order
+# Author: Martin Cusack
+
+#input a string
+sentence = ("The quick brown fox jumps over the lazy dog.")
+# use slice to output every second letter in reverse order
+print(sentence[::-2])
---
+++ Week03/testtypes.py
@@ -0,0 +1,3 @@
+i = 3
+print(i)
+print(type (i))
