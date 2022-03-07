56a18e39e6520ce8081cef272fb42675930f2bd9
martincusack979 <cusackmartin@gmail.com>
2022-03-07 18:04:26 +0000

    Square root

--- Week04/Week05/weekday.py
+++ Week04/Week05/weekday.py
@@ -1,9 +1,9 @@
 # Write a program that outputs whether or not today is a weekday
 #Author: Martin Cusack
 
-daysOfTheWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+daysOfTheWeek = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 today = input("Enter day of week:")
-if today == [0,1,2,3,4]:
+if today == ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday"):
   print("today is a weekday")
-elif today ==[5,6]:
+elif today ==("Saturday", "Sunday"):
     print("it's the weekend")
