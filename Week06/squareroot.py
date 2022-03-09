a4d0604e2b8141aa437d20e7ddfc1dd4db3067d6
martincusack979 <cusackmartin@gmail.com>
2022-03-09 14:21:03 +0000

    message

--- Week06/squareroot.py
+++
@@ -1,3 +0,0 @@
-# Write a program that takes a positive floating-point number as input and outputs an approximation of its square root
-# Author: Martin Cusack
-
---
+++ test.py
@@ -0,0 +1,5 @@
+a = 3
+print(a)
+b = [6,7,8]
+#print(b[1:2])
+print(b)
