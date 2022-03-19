893e24c47cb16e86542d538ffd1fb22665809d5c
martincusack979 <cusackmartin@gmail.com>
2022-03-19 15:59:47 +0000

    mobydick

--- mobydick.py
+++ mobydick.py
@@ -3,9 +3,9 @@
 
 # Write a program that reads in a text file and outputs the number of e's it contains.
 
-with open(r"C:\Users\cusac\OneDrive\Desktop\moby10b.txt" , 'r') as f:
+with open(r"C:\Users\cusac\OneDrive\Desktop\moby10b.txt" , 'r') as f:   # open document as f
     mobyDick = f.read()
     f.close ()
 
-number = mobyDick.count ('e')
+number = mobyDick.count ('e')   # use count () to get number of times "e" is used
 print (number)
